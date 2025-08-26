23aa7d66d6635f9dc6fb69f0db33749d699c2e69
Siddhant <siddhantkudale18@gmail.com>
2025-08-26 19:07:51 +0530

    Implemented AppModule

---
+++ app/src/main/java/sk/project/thenoteapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package sk.project.thenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import sk.project.thenoteapp.feature_note.data.data_source.NoteDatabase
+import sk.project.thenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import sk.project.thenoteapp.feature_note.domain.repository.NoteRepository
+import sk.project.thenoteapp.feature_note.domain.use_case.DeleteNote
+import sk.project.thenoteapp.feature_note.domain.use_case.GetNotes
+import sk.project.thenoteapp.feature_note.domain.use_case.NoteUseCase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCase {
+        return NoteUseCase(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
